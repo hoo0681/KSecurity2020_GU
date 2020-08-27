5022c51977800f725f7fdf85782613898ed8df58
hoo0681 <hoo0681@naver.com>
2020-08-27 18:10:04 +0900

    오류해결 확인

--- core/extractfeature.py
+++ core/extractfeature.py
@@ -85,6 +85,7 @@ class Extractor:
                     """
                     To input error class or function
                     """
+                   # raise
                     error += 1
                     continue
                 msg = queue.get()
